1ddddfdd911a1cca7b25473f67b1475f303e853c
Jeff Musa <jeffmusa@gmail.com>
2018-09-30 03:12:27 +0300

    gallery/urls.py

--- gallery/urls.py
+++ gallery/urls.py
@@ -9,7 +9,9 @@ urlpatterns=[
     url(r'^history/(\d{4}-\d{2}-\d{2})/$',views.past_uploads,name = 'pastUploads'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^$',views.all,name = 'all'),
-    url(r'^image/(\d+)',views.image,name ='image')
+    url(r'^image/(\d+)',views.image,name ='image'),
+    url(r'^location/(\d+)',views.locations,name='locations'),
+    url(r'^category/(\d+)',views.category,name='category'),
 
 ]
 
