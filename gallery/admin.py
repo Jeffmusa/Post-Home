0fc5f6ab31c923ff38d81f6cb2b0524d5bd3df7a
Jeff Musa <jeffmusa@gmail.com>
2018-09-30 03:08:55 +0300

    gallery/admin.py

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from .models import Editor,Category,Location,Image
 
-
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('category',)
 
 
 admin.site.register(Editor)
 admin.site.register(Category)
 admin.site.register(Location)
-admin.site.register(Image)
+admin.site.register(Image,ImageAdmin)
