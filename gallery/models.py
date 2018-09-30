a451a20818c310aee70fd0009b8cd5a6457e2562
Jeff Musa <jeffmusa@gmail.com>
2018-09-30 03:09:20 +0300

    gallery/models.py

--- gallery/models.py
+++ gallery/models.py
@@ -49,7 +49,7 @@ class Image(models.Model):
     name = models.CharField(max_length =30,null=True)
     description = models.TextField(null=True)
     location = models.ForeignKey(Location, null=True)
-    category = models.ForeignKey(Category, null=True)
+    category = models.ManyToManyField(Category)
     pub_date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to = 'images/')
 
@@ -81,7 +81,7 @@ class Image(models.Model):
         return uploads
 
     @classmethod
-    def search_image(cls,search_term):
+    def search_image_category(cls,search_term):
         uploads = cls.objects.filter(name__icontains=search_term)
         return uploads
        
