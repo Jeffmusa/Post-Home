00e64f477b8b66cbf481a2286d551b0a4b0a3bfd
Jeff Musa <jeffmusa@gmail.com>
2018-10-02 02:06:57 +0300

    styled

--- gallery/views.py
+++ gallery/views.py
@@ -70,14 +70,16 @@ def search_results(request):
 def locations(request,location_id):
     loc = Location.objects.get(name=location_id)
     location = Image.objects.filter(location=loc.id)
-    locat = Location.objects.all()
-    return render(request,'location.html',{"location":location,"locat":locat})
+    locs = Location.objects.all()
+    catego = Category.objects.all()
+    return render(request,'location.html',{"location":location,"catego":catego,"locs":locs})
 
 def category(request,category_id):
     cat = Category.objects.get(category_name=category_id)
     category = Image.objects.filter(category=cat.id)
     catego = Category.objects.all()
-    return render(request,'category.html',{"category":category,"catego":catego})
+    locs = Location.objects.all()
+    return render(request,'category.html',{"category":category,"catego":catego,"locs":locs})
 
 
 
@@ -85,15 +87,17 @@ def category(request,category_id):
 def image(request,image_id):
     
     image = Image.objects.filter(id = image_id)
-    for x in image:
-        print(x)
-    return render(request,"posts/image.html", {"image":image})
+    locs = Location.objects.all()
+    catego = Category.objects.all()
+
+    return render(request,"posts/image.html", {"image":image,"locs":locs,"catego":catego})
 
 
 def all(request):
     date = dt.date.today()
     catego = Category.objects.all()
+    locs = Location.objects.all()
     al = Image.objects.all()
 
-    return render(request,"posts/all.html", {"al":al, "date":date,"catego":catego})
+    return render(request,"posts/all.html", {"al":al, "date":date,"catego":catego,"locs":locs})
 
